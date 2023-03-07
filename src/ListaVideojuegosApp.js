2b83a7f6bc19973f6443f8940e835e2a48b14773
andrRT <44376940+andrRT@users.noreply.github.com>
2023-03-07 15:24:18 -0600

    solo faltan imagenes

--- src/ListaVideojuegosApp.js
+++ src/ListaVideojuegosApp.js
@@ -1,20 +1,39 @@
+import React from "react";
+
 const grupoVideoJuegos = [
-    {
-        nombre: "Hogwarts Legacy",
-        desarrollador: "Avalanche Software",
-        fechaLanzamiento: "10 de febrero de 2023",
-        img: harryp
-    },
-    {
-        nombre: "Super Smash Bros. Ultimate",
-        desarrollador: "Nintendo Entertainment",
-        fechaLanzamiento: "7 de diciembre de 2018",
-        img: mario
-    },
-    {
-        nombre: "Fall Guys",
-        desarrollador: "Mediatonic",
-        fechaLanzamiento: "4 de agosto de 2020",
-        img: fguys
-    }
+  {
+    nombre: "Hogwarts Legacy",
+    desarrollador: "Avalanche Software",
+    fechaLanzamiento: "10 de febrero de 2023",
+    img: "harryp.jpeg"
+  },
+  {
+    nombre: "Super Smash Bros. Ultimate",
+    desarrollador: "Nintendo Entertainment",
+    fechaLanzamiento: "7 de diciembre de 2018",
+    img: "smash.jpeg"
+  },
+  {
+    nombre: "Fall Guys",
+    desarrollador: "Mediatonic",
+    fechaLanzamiento: "4 de agosto de 2020",
+    img: "fallguys.jpeg"
+  }
 ];
+
+function Listavideojuegos() {
+  return (
+    <div>
+      {grupoVideoJuegos.map((videojuego) => (
+        <div key={videojuego.nombre}>
+          <h3>{videojuego.nombre}</h3>
+          <p>Desarrollador: {videojuego.desarrollador}</p>
+          <p>Fecha de lanzamiento: {videojuego.fechaLanzamiento}</p>
+          <img src={require(`./images/${videojuego.img}`).default} alt={videojuego.nombre} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Listavideojuegos;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import VideojuegosApp from './VideojuegosApp';
+// import './index.css';
+
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// //Utilizamos el JSX que devuelve el componente VideojuegosApp para injectar ese html
+// // en divRoot
+// root.render(<VideojuegosApp nombre='Meli'/>);
+
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import VideojuegosApp from './VideojuegosApp';
-import './index.css';
+import ListaVideojuegosApp from './ListaVideojuegosApp';
+import './list.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 //Utilizamos el JSX que devuelve el componente VideojuegosApp para injectar ese html
 // en divRoot
-root.render(<VideojuegosApp nombre='Yised'/>);
+root.render(<ListaVideojuegosApp/>);
